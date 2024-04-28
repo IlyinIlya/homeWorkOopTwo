ad04d61e0cd3cc1db17a0a93928556de01475e21
Ilyin Ilya <ilyin88ok@gmail.com>
2024-04-28 09:13:13 +0330

    1. Start 1.1 Add 4 Class 2. Add Interface 3. Delete Comments

--- src/Truck.java
+++ src/Truck.java
@@ -1,13 +1,7 @@
-public class Truck {
-    public String modelName;
-    public int wheelsCount;
+public class Truck extends Car {
 
-    public void updateTyre() {
-        System.out.println("Меняем покрышку");
-    }
-
-    public void checkEngine() {
-        System.out.println("Проверяем двигатель");
+    public Truck(String modelName, int wheelsCount) {
+        super(modelName, wheelsCount);
     }
 
     public void checkTrailer() {
